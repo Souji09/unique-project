26c5b303c97e984add25661685c38bc2e0892298
Bendy-Theink <thanhtungnguyennnn@gmail.com>
2024-08-13 15:13:18 +0700

    Phần Dialog của tùng và nam

---
+++ uniqueGame Projcect/Assets/Scripts/Dialogue.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Dialogue : MonoBehaviour
+{
+    public TextMeshProUGUI textComponent;
+    public string[] lines;
+    public float textSpeeds;
+
+    private int index;
+    // Start is called before the first frame update
+    void Start()
+    {
+        textComponent.text = string.Empty;
+        startDialogue();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            if (textComponent.text == lines[index])
+            {
+                NextLines();
+            }
+            else
+            {
+                StopAllCoroutines();
+                textComponent.text = lines[index];
+            }
+        }
+    }
+
+    void startDialogue()
+    {
+        index = 0;
+        StartCoroutine(TypeLine());
+
+    }
+
+    IEnumerator TypeLine()
+    {
+        foreach (char c in lines[index].ToCharArray())
+        {
+            textComponent.text += c;
+            yield return new WaitForSeconds(textSpeeds);
+        }
+    }
+    void NextLines()
+    {
+        if (index < lines.Length - 1)
+        {
+            index++;
+            textComponent.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
