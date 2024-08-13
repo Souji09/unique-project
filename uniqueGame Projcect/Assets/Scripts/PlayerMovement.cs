bda74a9288a8e2849cbd938363867827a70c264a
Ruan_Ren <souji2509@gmail.com>
2024-08-13 12:30:45 -0500

    Di chuyen nhan vat

---
+++ uniqueGame Projcect/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D rb;
+    private Vector2 Movement;
+    new Vector2 NormalizedSpeed;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement.x = Input.GetAxisRaw("Horizontal");
+        Movement.y = Input.GetAxisRaw("Vertical");
+
+        NormalizedSpeed = Movement.normalized;
+    }
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + speed * NormalizedSpeed * Time.deltaTime);
+    }
+}
